b44cda8d1db422e5babc848636ff16bb49ca78b4
harbenml <harbenml@pm.me>
2020-12-03 18:57:13 +0100

    add puzzle solution 1

---
+++ 01/puzzle01.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+data = pd.read_csv("data01.txt").values
+
+# part one
+for i in range(len(data)):
+    for j in range(i + 1, len(data)):
+        if data[i] + data[j] == 2020:
+            print(data[i] * data[j])
+
+# part two
+for i in range(len(data)):
+    for j in range(i + 1, len(data)):
+        for k in range(j + 1, len(data)):
+            if data[i] + data[j] + data[k] == 2020:
+                print(data[i] * data[j] * data[k])
