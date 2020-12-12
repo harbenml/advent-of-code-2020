091f90eecc7aca9c39a3b22a82919d7c02300191
harbenml <harbenml@pm.me>
2020-12-12 09:43:14 +0100

    add test to puzzle 01

--- puzzles/puzzle01.py
+++ puzzles/puzzle01.py
@@ -1,16 +1,27 @@
 import pandas as pd
-
-data = pd.read_csv("./data/data01.txt").values
+import numpy as np
 
 # part one
-for i in range(len(data)):
-    for j in range(i + 1, len(data)):
-        if data[i] + data[j] == 2020:
-            print(data[i] * data[j])
+def solve_part_1(data: np.ndarray) -> int:
+    for i in range(len(data)):
+        for j in range(i + 1, len(data)):
+            if data[i] + data[j] == 2020:
+                return int(data[i] * data[j])
+    return -1
+
 
 # part two
-for i in range(len(data)):
-    for j in range(i + 1, len(data)):
-        for k in range(j + 1, len(data)):
-            if data[i] + data[j] + data[k] == 2020:
-                print(data[i] * data[j] * data[k])
+def solve_part_2(data: np.ndarray) -> int:
+    for i in range(len(data)):
+        for j in range(i + 1, len(data)):
+            for k in range(j + 1, len(data)):
+                if data[i] + data[j] + data[k] == 2020:
+                    return int(data[i] * data[j] * data[k])
+    return -1
+
+
+if __name__ == "__main__":
+
+    data = pd.read_csv("./data/data01.txt").values
+    print(solve_part_1(data))
+    print(solve_part_2(data))
---
+++ tests/test_puzzle01.py
@@ -0,0 +1,15 @@
+from puzzles.puzzle01 import solve_part_1
+from puzzles.puzzle01 import solve_part_2
+
+import numpy as np
+import pandas as pd
+
+data = pd.read_csv("./data/data01.txt").values
+
+
+def test_solve_part_1(data: np.ndarray):
+    assert solve_part_1(data) == 970816
+
+
+def test_solve_part_2(data: np.ndarray):
+    assert solve_part_2(data) == 96047280
