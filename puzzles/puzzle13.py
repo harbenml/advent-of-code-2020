6e7d4362fe5138b3e5e52376231d2d52e1e111a5
harbenml <harbenml@pm.me>
2020-12-14 16:21:13 +0100

    clean code using mypy

--- puzzles/puzzle13.py
+++ puzzles/puzzle13.py
@@ -1,24 +1,25 @@
 from typing import Tuple
+from typing import List
 
 
-def get_data(filename: str) -> Tuple[int, list, list]:
+def get_data(filename: str) -> Tuple[int, List[int], List[int]]:
     with open(filename) as f:
         lines = f.read().split("\n")
 
     departure = int(lines[0])
-    bus_ids = lines[1].split(",")
-    delays = [i for i, el in enumerate(bus_ids) if not el == "x"]
-    bus_ids = [int(el) for el in bus_ids if not el == "x"]
+    bus_ids_raw = lines[1].split(",")
+    delays = [i for i, el in enumerate(bus_ids_raw) if not el == "x"]
+    bus_ids = [int(el) for el in bus_ids_raw if not el == "x"]
     return departure, bus_ids, delays
 
 
-def solve_part_1(bus_ids: list, departure: int) -> int:
+def solve_part_1(bus_ids: List[int], departure: int) -> int:
     wait_time = [(departure // el + 1) * el - departure for el in bus_ids]
     best_bus_id = bus_ids[wait_time.index(min(wait_time))]
     return best_bus_id * min(wait_time)
 
 
-def solve_part_2(bus_ids: list, departure: int, delays: list) -> int:
+def solve_part_2(bus_ids: List[int], departure: int, delays: list) -> int:
     delta_time = 1
     timestamp = 0
     for bus, delay in zip(bus_ids, delays):
--- tests/test_puzzle01.py
+++ tests/test_puzzle01.py
@@ -7,9 +7,9 @@ import pandas as pd
 data = pd.read_csv("./data/data01.txt").values
 
 
-def test_solve_part_1(data: np.ndarray):
+def test_solve_part_1():
     assert solve_part_1(data) == 970816
 
 
-def test_solve_part_2(data: np.ndarray):
+def test_solve_part_2():
     assert solve_part_2(data) == 96047280
