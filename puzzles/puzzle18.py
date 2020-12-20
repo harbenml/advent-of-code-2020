d7049e1fa019a6ca8241fdaf069bcfb332edb42c
harbenml <harbenml@pm.me>
2020-12-20 11:17:30 +0100

    add part 2 solution of day 18

--- puzzles/puzzle18.py
+++ puzzles/puzzle18.py
@@ -11,6 +11,9 @@ def get_data(filename: str) -> list:
     return open("./data/data18.txt").read().split("\n")
 
 
+""" PART 1 """
+
+
 class Number:
     def __init__(self, value):
         self.value = value
@@ -42,11 +45,43 @@ def solve_part1(filename: str) -> int:
     return result
 
 
-if __name__ == "__main__":
+""" PART 2 """
 
-    input_ = "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"
-    str_to_evaluate = parse(input_)
-    print(eval(str_to_evaluate).value)
+
+class Number2:
+    def __init__(self, value):
+        self.value = value
+
+    def __mul__(self, x):
+        return Number2(self.value + x.value)
+
+    def __sub__(self, x):
+        return Number2(self.value * x.value)
+
+
+def parse2(input_: str) -> str:
+    str_to_evaluate = ""
+    for x in input_:
+        if x in "0123456789":
+            str_to_evaluate += "Number2(" + x + ")"
+        else:
+            str_to_evaluate += x
+    str_to_evaluate = str_to_evaluate.replace("*", "-")
+    str_to_evaluate = str_to_evaluate.replace("+", "*")
+    return str_to_evaluate
+
+
+def solve_part2(filename: str) -> int:
+    data = get_data(filename)
+    result = 0
+    for exp in data:
+        str_to_evaluate = parse2(exp)
+        result += eval(str_to_evaluate).value
+    return result
+
+
+if __name__ == "__main__":
 
     filename = "./data/data18.txt"
     print(solve_part1(filename))
+    print(solve_part2(filename))
--- tests/test_puzzle18.py
+++ tests/test_puzzle18.py
@@ -1,5 +1,7 @@
 from puzzles.puzzle18 import Number
+from puzzles.puzzle18 import Number2
 from puzzles.puzzle18 import parse
+from puzzles.puzzle18 import parse2
 
 import pytest
 
@@ -17,4 +19,20 @@ import pytest
 )
 def test_parse(test_input, expected):
     str_to_evaluate = parse(test_input)
+    assert eval(str_to_evaluate).value == expected
+
+
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        ("1 + 2 * 3 + 4 * 5 + 6", 231),
+        ("1 + (2 * 3) + (4 * (5 + 6))", 51),
+        ("2 * 3 + (4 * 5)", 46),
+        ("5 + (8 * 3 + 9 + 3 * 4 * 3)", 1445),
+        ("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 669060),
+        ("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 23340),
+    ],
+)
+def test_parse2(test_input, expected):
+    str_to_evaluate = parse2(test_input)
     assert eval(str_to_evaluate).value == expected
