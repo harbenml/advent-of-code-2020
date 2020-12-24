a54e2a55fff00ea38a3caf1a993e249110f72567
harbenml <harbenml@pm.me>
2020-12-24 10:22:56 +0100

    add test of day 20 part 2

--- tests/test_puzzle20.py
+++ tests/test_puzzle20.py
@@ -1,9 +1,15 @@
 from puzzles.puzzle20 import get_data
 from puzzles.puzzle20 import solve_part1
+from puzzles.puzzle20 import solve_part2
+
+fn = "./data/test_data20.txt"
 
 
 def test_solve_part1():
-    fn = "./data/test_data20.txt"
-    tiles = get_data(fn)
-    result = solve_part1(tiles)
+    result = solve_part1(fn)
     assert result == 20899048083289
+
+
+def test_solve_part2():
+    result = solve_part2(fn)
+    assert result == 273
