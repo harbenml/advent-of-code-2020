1180902bf03d4e7c1e65cdb7af4ec5bdee563eb1
harbenml <harbenml@pm.me>
2020-12-26 09:16:30 +0100

    get dict for allergens vs ingerdients

--- puzzles/puzzle21.py
+++ puzzles/puzzle21.py
@@ -7,6 +7,17 @@ print(data)
 foods = [el.split(" (contains ")[0].split(" ") for el in data]
 print(foods)
 
-allergs = [el.split(" (contains ")[1].split(")")[0].split(" ") for el in data]
+allergs = [el.split(" (contains ")[1].split(")")[0].split(", ") for el in data]
 print(allergs)
 
+d = {}
+idx = -1
+for food in foods:
+    idx += 1
+    for al in allergs[idx]:
+        print(al, food)
+        if al not in d:
+            d[al] = set(food)
+        else:
+            d[al] = d[al] & set(food)
+print(d)
