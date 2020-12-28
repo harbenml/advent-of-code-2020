6f4b3b0c2d42fdbb7186bede6c40088653a863c8
harbenml <harbenml@pm.me>
2020-12-28 15:30:24 +0100

    add tests and part 2 solution day 22

--- puzzles/puzzle22.py
+++ puzzles/puzzle22.py
@@ -36,16 +36,53 @@ def play_game(p1: Deque, p2: Deque) -> int:
     while True:
         p1, p2 = play_next_round(p1, p2)
         if not p1 or not p2:
-            print("game finished.")
             if not p2:
                 return calc_score(p1)
             else:
                 return calc_score(p2)
 
 
+def play_recursive_game(p1: Deque, p2: Deque) -> Tuple[int, int]:
+    prev = set()
+    while True:
+        state = (tuple(p1), tuple(p2))
+        if state in prev:
+            # player 1 wins
+            return 1, calc_score(p1)
+        else:
+            prev.add(state)
+            c1, c2 = p1.popleft(), p2.popleft()
+            if len(p1) >= c1 and len(p2) >= c2:
+                if not p1:
+                    return 2, calc_score(p1)
+                elif not p2:
+                    return 1, calc_score(p1)
+                else:
+                    p1sub, p2sub = list(p1)[:c1], list(p2)[:c2]
+                    winner, _ = play_recursive_game(deque(p1sub), deque(p2sub))
+            else:
+                winner = 1 if c1 > c2 else 2
+
+        if winner == 1:
+            p1.extend([c1, c2])
+        else:
+            p2.extend([c2, c1])
+
+        if not p1:
+            return 2, calc_score(p2)
+        elif not p2:
+            return 1, calc_score(p1)
+
+
 def solve_part1(fn: str):
     p1, p2 = get_data(fn)
     score = play_game(p1, p2)
+    return score
+
+
+def solve_part2(fn: str):
+    p1, p2 = get_data(fn)
+    _, score = play_recursive_game(p1, p2)
     return score
 
 
@@ -56,3 +93,5 @@ if __name__ == "__main__":
     score = solve_part1(fn)
     print("solution part 1:", score)
 
+    score2 = solve_part2(fn)
+    print("solution part 2:", score2)
--- tests/test_puzzle22.py
+++ tests/test_puzzle22.py
@@ -1,14 +1,14 @@
 from puzzles.puzzle22 import solve_part1
+from puzzles.puzzle22 import solve_part2
 
-# from puzzles.puzzle22 import solve_part2
+fn = "./data/test_data22.txt"
 
 
 def test_solve_part1():
-    fn = "./data/test_data22.txt"
     result = solve_part1(fn)
     assert result == 306
 
 
-# def test_solve_part2():
-#     result = solve_part2(fn)
-#     assert result == 273
+def test_solve_part2():
+    result = solve_part2(fn)
+    assert result == 291
